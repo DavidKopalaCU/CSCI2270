3eb4004b5de21dffda63b5b7b418b63eb2647882
David Kopala <kopala.david@gmail.com>
2018-01-24 15:27:40 -0700

    Updated assignment desc; Updated program to take command line args; Added comments

--- Assignment1/Assignment1/Assignment1/main.cpp
+++ Assignment1/Assignment1/Assignment1/main.cpp
@@ -11,8 +11,20 @@
 
 using namespace std;
 
+//The maximum number of lines in the text file
 #define MAX_LENGTH      100
 
+/*
+ split description
+ 
+ splits a line from the file into components
+ that can be parsed and used to populate a struct
+ 
+ Inputs: string fill[3] -> the array to populate
+         string line -> the string to parse
+ Outputs: bool -> if the operation was successful or not
+ 
+ */
 bool split(string fill[3], string line) {
     int index = 0;
     string temp = "";
@@ -32,13 +44,16 @@ bool split(string fill[3], string line) {
     return true;
 }
 
+//Represents an item on the community board from the input file
 struct CommunityItem {
-    string type;
-    bool forSale;
-    int price;
+    string type;    //The type of item
+    bool forSale;   //If the item is for sale or wanted
+    int price;      //The associated price
     
+    //Default Constructor
     CommunityItem() {type = ""; forSale = false; price = -1;};
     
+    //Instantiates an item based on a line from the file
     CommunityItem(string line) {
         string comps[3];
         split(comps, line);
@@ -53,6 +68,20 @@ struct CommunityItem {
     }
 };
 
+/*
+ deleteItem description
+ 
+ deletes an item from the array of items at a specific index
+ 
+ does this by shifting elements up to fill the deleted spot,
+ then replacing the last item (which has been duplicated) with
+ a nullptr
+ 
+ THIS ONLY WORKS BECAUSE THE ARRAY IS AN ARRAY OF POINTERS
+ 
+ Inputs: CommunityItem *arr[] -> The array of items
+         int index -> the index to delete
+ */
 void deleteItem(CommunityItem *arr[MAX_LENGTH], int index) {
     for (int i = index; i < MAX_LENGTH - 1; i++) {
         if (arr[i+1] != nullptr) {
@@ -64,6 +93,22 @@ void deleteItem(CommunityItem *arr[MAX_LENGTH], int index) {
     }
 }
 
+/*
+ foundItem description
+ 
+ helper function that manages what happens when a match is found
+ 
+ prints out the required text to the console
+ deletes the found item
+ decrement the current number of items
+ 
+ Inputs: bool *found -> Points to the found boolean in the while loop of main()
+         int curLen -> Points to the curLen int the main()
+         CommunityItem *sale -> the item that is being sold
+         CommunityItem *wanted -> the item that is wanted
+         int index -> the index of the stored item in the array
+         CommunityItem *items -> A pointer to the array of stored items
+ */
 void foundItem(bool *found, int *curLen, CommunityItem *sale, CommunityItem *wanted, int index, CommunityItem *items[MAX_LENGTH]) {
     *found = true;
     cout<<sale->type<<" "<<wanted->price<<endl;
@@ -77,23 +122,36 @@ int main(int argc, const char * argv[]) {
     int currLen = 0;
     //cout<<items[0].price<<endl;
     
+    //Get the filename from the user
     string filename;
-    getline(cin, filename);
+    if (argc != 2) {
+        cout<<"Incorrent Usage!"<<endl;
+        cout<<"Usage: ./a.out [filepath]"<<endl;
+        filename = "/Users/davidkopala/Documents/CUBoulder/CSCI2270/Assignment1/garageSale.txt";
+    } else {
+        filename = argv[1];
+    }
+    //getline(cin, filename);
     ifstream file(filename);
     
+    //Iterate through the file line by line
     string line;
     while (!file.eof()) {
         getline(file, line);
         
+        //Specifies if there is a matching item in the array
         bool found = false;
         
+        //Creates a new item based on the current line in the file
         CommunityItem *item = new CommunityItem(line);
+        //Search the stored items for a match
         for (int i = 0; i < currLen; i++) {
+            //Check if they have a matching type
             if (item->type != items[i]->type) {
                 continue;
             }
             if (item->forSale) {
-                if (!items[i]->forSale) {
+                if (!items[i]->forSale) {   //Make sure they are of different sale types
                     // SALE            WANTED
                     if (item->price <= items[i]->price) {
                         //FOUND ITEM
@@ -104,7 +162,7 @@ int main(int argc, const char * argv[]) {
                     continue;
                 }
             } else {
-                if (items[i]->forSale) {
+                if (items[i]->forSale) {    //Make sure they are of different sale types
                     if (item->price >= items[i]->price) {
                         //FOUND ITEM
                         foundItem(&found, &currLen, item, items[i], i, items);
@@ -117,12 +175,14 @@ int main(int argc, const char * argv[]) {
         }
         
         if (!found) {
+            //Add the item to the array
             items[currLen++] = item;
         }
     }
     
     cout<<"#"<<endl;
     
+    //Print out the remaining items
     for (int i = 0; i < currLen; i++) {
         cout<<items[i]->type;
         if (items[i]->forSale) {
